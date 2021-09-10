6202bb58b741fa91ba7bcfb4461b313612464579
fstrug <fstrug2@uic.edu>
2021-09-10 10:41:50 -0500

    Added a function to calculate the two body decay for some particle.

--- particle_class.py
+++ particle_class.py
@@ -9,10 +9,11 @@ import numpy as np
 import time 
 from helper import *
 
+
 ############################Create particle class object
 
 class particle_lorentz:
-    def __init__(self, four_vector, tag, decay_dictionary):
+    def __init__(self, four_vector, tag, decay_dictionary, mass_dictionary):
         self.four_vector = four_vector
         #Momentum Attributes
         self.Px = four_vector[1]
@@ -27,6 +28,7 @@ class particle_lorentz:
         self.V = self.Pmag / self.E
         self.tag = tag
         self.decay_dictionary = decay_dictionary
+        self.mass_dictionary = mass_dictionary
         
     #Takes list of avaiable mass states and see which pairs are accessible
     def check_decay(self, mass_states):
@@ -63,10 +65,37 @@ class particle_lorentz:
     def two_body_decay(self, mode = None):
         #Select decay mode
         if mode == None:
-            particles = select_decay_branch()
+            particles = self.select_decay_branch()
         else: 
             particles = mode
         
+        ##Calculate four vectors
+        mass_1 = mass_dictionary[particles[0]]
+        mass_2 = mass_dictionary[particles[1]]
+        particle_1_pmag = calc_mag_p1(self.M, mass_1, mass_2)
+        particle_2_pmag = particle_1_pmag
+        particle_1_E = np.sqrt(mass_1**2 + particle_1_pmag**2)
+        particle_2_E = np.sqrt(mass_2**2 + particle_2_pmag**2)
+        #Decay isotropically
+        phi = np.random.uniform(0, 2*np.pi)
+        theta = np.arccos(np.random.uniform(-1, 1))
+        particle_1_px = particle_1_pmag * np.sin(theta) * np.cos(phi)
+        particle_1_py = particle_1_pmag * np.sin(theta) * np.sin(phi)
+        particle_1_pz = particle_1_pmag * np.cos(theta)
+        particle_1_four_vector = np.array([particle_1_E, particle_1_px, particle_1_py, particle_1_pz])
+        particle_2_four_vector = np.array([particle_2_E, -particle_1_px, -particle_1_py, -particle_1_pz])
+        #Find Lorentz Boost into lab-frame
+        self_four_vector_rest , boost_matrix = boost_4_vector_rest(self.four_vector)
+        boost_matrix_rotated = np.linalg.inv(boost_matrix)
+        #boost into lab frame
+        particle_1_four_vector_lab = np.matmul(boost_matrix_rotated, particle_1_four_vector)
+        particle_2_four_vector_lab = np.matmul(boost_matrix_rotated, particle_2_four_vector)
+        
+        #Create particle objects
+        particle_1 = particle_lorentz(particle_1_four_vector_lab, particles[0], self.decay_dictionary, self.mass_dictionary)
+        particle_2 = particle_lorentz(particle_2_four_vector_lab, particles[1], self.decay_dictionary, self.mass_dictionary)
+        
+        return(particle_1, particle_2)
         
         
     
@@ -84,12 +113,12 @@ decay_dictionary = {
             (0.0, "fake", "fake", "fake")]
     }
 mass_dictionary = {
-    "top"   : 173.29,
+    "top"   : 172.76,
     "W"     : 80.369,
-    "bottom": 1.14
+    "bottom": 4.18
     }
     
-top = particle_lorentz([10,0,0,0], "top")
+top = particle_lorentz([173.049,10,0,0], "top", decay_dictionary, mass_dictionary)
 
 
 
